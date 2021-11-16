2dbdb89c06d8ca3b0c4b7cec991d25c835a69df2
RandilCPiumantha <cst17009@std.uwu.ac.lk>
2021-11-16 11:05:58 +0530

    flutter_mapadded

--- lib/main.dart
+++ lib/main.dart
@@ -1,71 +1,49 @@
-/// Flutter code sample for AnimatedBuilder
-
-// This code defines a widget that spins a green square continually. It is
-// built with an [AnimatedBuilder] and makes use of the [child] feature to
-// avoid having to rebuild the [Container] each time.
-
-import 'dart:math' as math;
-
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong/latlong.dart';
 
-void main() => runApp(const MyApp());
 
-/// This is the main application widget.
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  static const String _title = 'Flutter Code Sample';
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: _title,
-      home: MyStatefulWidget(),
-    );
-  }
+void main() {
+  runApp(new MaterialApp(
+    home: new MyApp(),
+  ));
 }
 
-/// This is the stateful widget that the main application instantiates.
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key? key}) : super(key: key);
-
+class MyApp extends StatefulWidget {
   @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+  _State createState() => new _State();
 }
 
-/// This is the private State class that goes with MyStatefulWidget.
-/// AnimationControllers can be created with `vsync: this` because of TickerProviderStateMixin.
-class _MyStatefulWidgetState extends State<MyStatefulWidget>
-    with TickerProviderStateMixin {
-  late final AnimationController _controller = AnimationController(
-    duration: const Duration(seconds: 10),
-    vsync: this,
-  )..repeat();
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
+class _State extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: _controller,
-      child: Container(
-        width: 200.0,
-        height: 200.0,
-        color: Colors.green,
-        child: const Center(
-          child: Text('Whee!'),
-        ),
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Name here'),
       ),
-      builder: (BuildContext context, Widget? child) {
-        return Transform.rotate(
-          angle: _controller.value * 2.0 * math.pi,
-          child: child,
-        );
-      },
+      body: new Container(
+          padding: new EdgeInsets.all(32.0),
+          child: new Center(
+            child: new Column(
+              children: <Widget>[
+                new Flexible(
+                    child: new FlutterMap(
+                      options: new MapOptions(
+                          // center: new LatLng(41.8781, -87.6298),
+                          zoom: 10.0
+                      ),
+                      layers: [
+                        new TileLayerOptions(
+                          urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                          subdomains: ['a','b','c'],
+                        )
+                      ],
+                    )
+                )
+              ],
+            ),
+          )
+      ),
     );
   }
 }
